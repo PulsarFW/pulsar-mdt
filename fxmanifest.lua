b506603b9ebcd0fa2282125f9d3c3deb37cc6e9b
Artmines <arturowells@gmail.com>
2026-08-02 08:16:03 -0230

    fix(fxmanifest):improve fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,23 +1,28 @@
-fx_version("cerulean")
-game("gta5")
+fx_version 'cerulean'
+game 'gta5'
 
-name("Pulsar Mdt")
-description("Police Mobile Data Terminal")
-author("Artmines - maintained for Pulsar Framework")
-url("https://pulsarframe.work")
-version("v1.0.0")
+name 'Pulsar Mdt'
+description 'Police Mobile Data Terminal'
+author 'Artmines - maintained for Pulsar Framework'
+url 'https://pulsarframe.work'
+version 'v1.0.0'
 
-version_check("yes")
-github("https://github.com/PulsarFW/pulsar_mdt")
-lua54("yes")
+version_check 'yes'
+github 'https://github.com/PulsarFW/pulsar_mdt'
 
-client_script("@pulsar_core/components/cl_error.lua")
-shared_script("@pulsar_core/core/sh_pulsar.lua")
-client_script("@pulsar_pwnzor/client/check.lua")
-server_script("@oxmysql/lib/MySQL.lua")
+client_script '@pulsar_core/components/cl_error.lua'
+shared_script '@pulsar_core/core/sh_pulsar.lua'
+client_script '@pulsar_pwnzor/client/check.lua'
+server_script '@oxmysql/lib/MySQL.lua'
 
-client_scripts({ "client/**/*.lua" })
-server_scripts({ "config/utils/*.lua", "server/**/*.lua" })
+client_scripts({ 
+    "client/**/*.lua" 
+})
+server_scripts({ 
+    "config/utils/*.lua", 
+    "server/**/*.lua" 
+})
 
-ui_page("ui/dist/index.html")
+ui_page 'ui/dist/index.html'
 files({ "ui/dist/index.html", "ui/dist/assets/*", "config/shared.lua" })
+lua54 'yes'
